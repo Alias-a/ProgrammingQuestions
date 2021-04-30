392f599df05034160c70901e6b2bd586db2652ad
Justin Gregorio <jusgregoriotin@gmail.com>
2021-04-30 12:18:38 -0600

    Got tests working.

--- src/ProgrammingQuestions.js
+++ src/ProgrammingQuestions.js
@@ -6,9 +6,11 @@ export class ProgrammingQuestions {
         let firstChar = string.charAt(0);
         let lastChar = string.charAt(string.length - 1)
         if (string.length > 2) {
-            return firstChar == lastChar && this.isPalindrome(string.substring(1, string.length - 2));
+            return firstChar === lastChar && this.isPalindrome(string.substring(1, string.length - 2));
         } else {
-            return firstChar == lastChar;
+            return firstChar === lastChar;
         }
     }
 }
+
+export function test() { return "test"; }
--- test/ProgrammingQuestions.test.js
+++ test/ProgrammingQuestions.test.js
@@ -1,9 +1,9 @@
-import { ProgrammingQuestions } from "../code/programming_questions";
+import { ProgrammingQuestions } from "../src/ProgrammingQuestions";
 
 describe('isPalindrome', () => {
     it('should return true when the passed string is a palindrome', () => {
         const result = ProgrammingQuestions.isPalindrome('racecar');
-        expect(value).toBe(true)
+        expect(result).toBe(true);
     })
 
 })
